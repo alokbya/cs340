d9c76dd4b92effccdb820db1c48783a711870287
Alaaddin Hiefield <alaaddin.hiefield@gmail.com>
2024-04-25 22:10:48 -0700

    starting activity5

---
+++ activity5/activity-m3-1.sql
@@ -0,0 +1,4 @@
+-- select the columns ProductNumber, ProductName and ListPrice
+-- and sort by ListPrice in descending order (most expensive first)
+SELECT ProductNumber, ProductName, ListPrice
+ORDER BY ListPrice DESC;
