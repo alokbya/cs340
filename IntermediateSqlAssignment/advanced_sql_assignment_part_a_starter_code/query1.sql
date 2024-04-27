ec2dbc2f9c66c817f74202be92067f04c558f600
alaaddin hiefield <alaaddin.hiefield@gmail.com>
2024-04-26 22:56:53 -0600

    finish intermediate sql assignment

--- IntermediateSqlAssignment/advanced_sql_assignment_part_a_starter_code/query1.sql
+++ IntermediateSqlAssignment/advanced_sql_assignment_part_a_starter_code/query1.sql
@@ -1,5 +1,41 @@
 -- Write the query to create the 4 tables below.
+CREATE TABLE `client` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `first_name` varchar(255) NOT NULL,
+ `last_name` varchar(255) NOT NULL,
+ `email` varchar(255) NOT NULL,
+ PRIMARY KEY (`id`),
+ CONSTRAINT `full_name` UNIQUE (`first_name`, `last_name`)
+) ENGINE=InnoDB;
 
+CREATE TABLE `employee` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `first_name` varchar(255) NOT NULL,
+ `last_name` varchar(255) NOT NULL,
+ `start_date` date NOT NULL,
+ `email` varchar(255) NOT NULL,
+ PRIMARY KEY (`id`),
+ CONSTRAINT `full_name` UNIQUE (`first_name`, `last_name`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `project` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `title` varchar(255) NOT NULL,
+ `comments` text,
+ `cid` int(11),
+ PRIMARY KEY (`id`),
+ UNIQUE KEY `unique_title` (`title`),
+ FOREIGN KEY (`cid`) REFERENCES `client`(`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `works_on` (
+ `pid` int(11) NOT NULL,
+ `eid` int(11) NOT NULL,
+ `due_date` date NOT NULL,
+ PRIMARY KEY (`eid`, `pid`),
+ FOREIGN KEY (`pid`) REFERENCES `project`(`id`),
+ FOREIGN KEY (`eid`) REFERENCES `employee`(`id`)
+) ENGINE=InnoDB;
 
 
 -- Leave the queries below untouched. These are to test your submission correctly.
--- IntermediateSqlAssignment/advanced_sql_assignment_part_a_starter_code/query2.sql
+++ IntermediateSqlAssignment/advanced_sql_assignment_part_a_starter_code/query2.sql
@@ -1,4 +1,27 @@
 -- write your queries to insert data here
+INSERT INTO `client`(first_name, last_name, email) 
+VALUES 
+    ('Sara', 'Smith', 'smiths@hello.com'),
+    ('Miguel', 'Cabrera', 'mc@hello.com'),
+    ("Bo", "Chan'g", "bochang@hello.com");
+
+INSERT INTO `employee`(first_name, last_name, start_date, email) 
+VALUES 
+    ('Ananya', 'Jaiswal', '2008-04-10', 'ajaiswal@hello.com'),
+    ('Michael', 'Fern', '2015-07-19', 'michaelf@hello.com'),
+    ("Abdul", "Rehman", "2018-02-27", "rehman@hello.com");
+
+INSERT INTO `project`(cid, title, comments) 
+VALUES 
+    ((SELECT client.id FROM client WHERE first_name = 'Sara' and last_name = 'Smith'), 'Diamond', 'Should be done by Jan 2019'),
+    ((SELECT client.id FROM client WHERE first_name = 'Bo' and last_name = "Chan'g"), "Chan'g", "Ongoing maintenance"),
+    ((SELECT client.id FROM client WHERE first_name = 'Miguel' and last_name = "Cabrera"), "The Robinson Project", NULL);
+
+INSERT INTO `works_on`(eid, pid, due_date)
+VALUES 
+    ((SELECT employee.id FROM employee WHERE first_name = 'Ananya' and last_name = 'Jaiswal'), (SELECT project.id FROM project WHERE title = "Chan'g"), '2020-11-19'),
+    ((SELECT employee.id FROM employee WHERE first_name = 'Michael' and last_name = 'Fern'), (SELECT project.id FROM project WHERE title = "The Robinson Project"), '2020-12-05'),
+    ((SELECT employee.id FROM employee WHERE first_name = 'Abdul' and last_name = 'Rehman'), (SELECT project.id FROM project WHERE title = "Diamond"), '2021-01-01');
 
 
 -- Leave the queries below untouched. These are to test your submission correctly.
